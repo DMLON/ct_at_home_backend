413b3e895c9954745baca4376f89f8187cf58fa8
Damian Deza <damiandeza@gmail.com>
2021-10-02 03:43:40 -0300

    Updated mongo product services

---
+++ backend/src/services/product.services.mongo.ts
@@ -0,0 +1,61 @@
+import { productModel } from "../models/product.model";
+
+export async function createProduct(data) {
+	try {
+		const exist = await productModel.findOne({ code: data.code })
+		if (exist) {
+			throw new Error(`Product ${data.code} already exists`)
+		}
+		const response = await productModel.create({timestamp:new Date(), ...data})
+		return response
+	} catch (error:any) {
+		throw new Error(error)
+	}
+}
+
+export async function getProduct(code) {
+	try {
+		const product = await productModel.find({ code })
+		if (!product) {
+			throw new Error('Product not found')
+		}
+		return product
+	} catch (error:any) {
+		throw new Error(error)
+	}
+}
+
+export async function getAllProducts() {
+	try {
+		const products = await productModel.find({});
+		return products;
+	} catch (error:any) {
+		throw new Error(error)
+	}
+}
+
+export async function deleteProduct(code){
+    try {
+		const exist = await productModel.findOne({ code: code })
+		if (!exist) {
+			throw new Error(`Product ${code} does not exist`)
+		}
+		const res = await productModel.deleteOne({code});
+		return res;
+	} catch (error:any) {
+		throw new Error(error)
+	}
+}
+
+export async function updateProduct(code,data){
+    try {
+		const exist = await productModel.findOne({ code: data.code })
+		if (!exist) {
+			throw new Error(`Product ${data.code} does not exist`)
+		}
+		const product = await productModel.updateOne({code},{$set:{...data}});
+		return product;
+	} catch (error:any) {
+		throw new Error(error)
+	}
+}
