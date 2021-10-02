4176dc06896aa9ae3d092f6038551f257bd42d45
Damian Deza <damiandeza@gmail.com>
2021-10-02 03:55:15 -0300

    Added comment on index.ts for mongo and firebase switching

--- backend/src/services/index.ts
+++ backend/src/services/index.ts
@@ -1,2 +1,5 @@
 export * as cartService from "./cart.services.firebase"
 export * as productService from "./products.services.firebase"
+
+// export * as cartService from "./cart.services.mongo"
+// export * as productService from "./products.services.mongo"
